8fb682d2f8a6af92fb4471cead1a1d9e1324761f
Carlos Raúl Marmolejo Sánchez <raul.marmolejo1487@gmail.com>
2020-02-29 20:58:07 -0600

    Change from md to vue

---
+++ src/components/articles/MostUsefulGitCommands.vue
@@ -0,0 +1,38 @@
+<template>
+    <b-container class="most-useful-git-commands">
+        <b-row class="text-center">
+            <b-col sm>
+                <h2>10 Git Commands That Every Hiring Manager Should Ask</h2>
+                <p><em>It may be seem trivial, but every developer should know this commands or at least have an idea</em></p>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col sm="6" class="left-separator">
+                <img src="../../assets/branching-illustration.png" alt="Image of Git Branching" class="img-fluid">
+            </b-col>
+            <b-col sm="6" class="left-separator">
+                <p align="justify">Let's start with the commands, then some examples and finally with some use cases.</p>
+                <h3> git reset --hard </h3>
+                <p align="justify"> If you're not working directly on your local git repository, then this command is pretty useful and <b>very safe</b>. The syntax is as follows:<br><br>
+                <code>git reset --hard [some commit]</code>
+                <br><br>Where <code>commit</code> 
+            </p> 
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+    name: 'MostUsefulGitCommands',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@media screen {
+    .most-useful-git-commands{
+        padding: 30px;
+    }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import MostRecentEntries from './components/MostRecentEntries.vue'
-import MostUsefulGitCommands from './components/articles/MostUsefulGitCommands.md'
+import MostUsefulGitCommands from './components/articles/MostUsefulGitCommands.vue'
 
 Vue.use(VueRouter)
 Vue.config.productionTip = false
